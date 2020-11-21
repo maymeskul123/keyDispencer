6cae3c324a5028f0d9091ddcd1f8071bd3398882
Vitalik <maymeskul@gmail.com>
2020-11-21 21:10:41 +0200

    Fix_programm

--- window.py
+++ window.py
@@ -5,11 +5,15 @@ import os, datetime, getpass
 class Window:
     def __init__(self, master):
         self.master = master
+        #self.pattern_key = '\d\d\d\ \d\d\d\ \d\d\d\ \d\d\d'
         self.master.title('Test for UBISOFT')
         self.master.geometry('600x300+200+100')
         self.choiceCombo = False
-        self.directory = os.getcwd() + '\\unused'
-        self.files = os.listdir(self.directory)
+        self.found_key = False
+        self.current_given_file = ''
+        self.directory_unused = os.getcwd() + '\\unused'
+        self.directory_used = os.getcwd() + '\\used'
+        self.files = os.listdir(self.directory_unused)
         self.files_names = [name.split('.')[0] for name in self.files]
         self.labKeysBatch = tk.Label(self.master, text='Keys Batch').place(x=10, y=10, width=80)
         self.combobox = ttk.Combobox(self.master, values=self.files_names, state='readonly')
@@ -31,32 +35,39 @@ class Window:
         self.choiceCombo = True
         f = open(self.get_choice_file(), 'r')
         line = f.readline()
+        line = self.normalizeLine(line)
         self.text1.insert(1.0, line)
         f.close()
 
     def pressGet(self, event):
-        if self.choiceCombo:
+        code = self.text1.get(1.0, tk.END)
+        if self.choiceCombo and self.found_key and code !='\n' and code !='':
             file_name = self.combobox.get()
-            new_f_name = self.directory + '\\' + file_name + '_new.txt'
-            used_file = os.getcwd() + '\\used\\' + file_name + '_used.txt'
-            time_format = "%Y-%m-%d %H:%M:%S"
-            with open(self.get_choice_file(), 'r') as f, open(new_f_name, 'w') as f1:
-                line = f.readline()
-                used_f = open(used_file, 'a')
-                date = datetime.datetime.today()
-                if line[len(line) - 1:len(line)] == '\n':
-                    line = line[:len(line) - 1]
-                # if line[len(line) - 1:len(line)] != '\n':
-                #     line = line[:len(line)] + '\n'
-                used_f.writelines(line + '-' + getpass.getuser() + '-' + f"{date:{time_format}}" + '\n')
-                used_f.close()
-                for line in f:
-                    f1.write(line)
-            f.close()
-            f1.close()
-            os.remove(self.get_choice_file())
-            os.rename(new_f_name, self.get_choice_file())
+            used_file = self.directory_used + '\\' + file_name + '_used.txt'
+            self.del_line_unused(file_name, self.check_given_away_code(code[:len(code) - 1], used_file), used_file)
             self.choiceCombobox('')
+
+    def del_line_unused(self, file_name, add_to_used, used_file):
+        new_f_name = self.directory_unused + '\\' + file_name + '_new.txt'
+        with open(self.get_choice_file(), 'r') as f, open(new_f_name, 'w') as f1:
+            line = f.readline()
+            for line_f in f:
+                f1.write(line_f)
+        f.close()
+        f1.close()
+        os.remove(self.get_choice_file())
+        os.rename(new_f_name, self.get_choice_file())
+        if add_to_used:
+            self.app_to_used(used_file, self.normalizeLine(line))
+
+    def app_to_used(self, used_file, line):
+        time_format = "%Y-%m-%d %H:%M:%S"
+        used_f = open(used_file, 'a')
+        date = datetime.datetime.today()
+        # if line[len(line) - 1:len(line)] == '\n':
+        #     line = line[:len(line) - 1]
+        used_f.writelines(line + '-' + getpass.getuser() + '-' + f"{date:{time_format}}" + '\n')
+        used_f.close()
 
     def pressCancel(self, event):
         self.master.destroy()
@@ -65,5 +76,23 @@ class Window:
         file_name = self.combobox.get()
         index_f = self.files_names.index(file_name)
         nameFiles = (self.files[index_f])
-        old_f_name = self.directory + '\\' + nameFiles
+        old_f_name = self.directory_unused + '\\' + nameFiles
         return old_f_name
+
+    def normalizeLine(self, line):
+        if line.find('\n') > 0:
+            line = line.replace('\n', '')
+        if line.find('-') > 0:
+            line = line.replace('-', ' ')
+        self.found_key = True
+        return line
+
+    def check_given_away_code(self, code, used_f):
+        key_add = True
+        if os.path.exists(used_f):
+            with open(used_f, 'r') as f:
+                for line in f:
+                    if line[:15] == code:
+                        key_add = False
+                        break
+        return key_add
